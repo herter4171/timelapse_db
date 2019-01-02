bc41d3b90b33634b6afc3bc87b5ded4578329a15
herter4171 <nonyabusiness@nothing.org>
2019-01-01 21:28:25 -0500

    Fixed burning bash.

--- raspbian_install/burn.sh
+++ raspbian_install/burn.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+source env.sh
+
+export ETCHER_DISK=/dev/sda
+
+#sudo $ETCHER_BINARY -d $ETCHER_DISK $(ls downloads/*.img)
+
+# Mount boot partition
+if [ ! -d boot ]; then mkdir boot; fi
+sudo mount ${ETCHER_DISK}1 boot
+
+sudo printf "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+network={
+    ssid=\"$(cat .wifi_ssid)\"
+    psk=\"$(cat .wifi_password)\"
+    key_mgmt=WPA-PSK
+}" > wpa_supplicant.conf
+
+if [ $(cat /proc/mounts | grep -c $PWD/boot) -gt 0 ]; then 
+    cd boot
+    sudo touch ssh
+    mv ../wpa_supplicant.conf .
+    cd ..
+    sudo umount boot
+fi
--- raspbian_install/env.sh
+++ raspbian_install/env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export RASPBIAN_VERSION="raspbian_lite_latest"
 export ETCHER_VERSION="1.4.9"
-export ETCHER_BINARY="$(ls downloads/balena-etcher-cli*-dist/balena-etcher)"
+export ETCHER_BINARY="$PWD/downloads/balena-etcher-cli-$ETCHER_VERSION-linux-x64-dist/balena-etcher"
+
